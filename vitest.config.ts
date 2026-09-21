96762c71db4af1c4f7213084c59c89d868d67b4b
Wilson Faustino <wilson.rfaustino@gmail.com>
2026-09-20 21:16:12 -0300

    chore(test): exclude storybook files from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       exclude: [
         '**/*.test.{ts,tsx}',
         '**/*.mock.{ts,tsx}',
+        '**/*.stories.{ts,tsx}',
         '**/test/**',
         '**/types/**',
         'src/main.tsx',
